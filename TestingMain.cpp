4881ddb958424b3539ae458c623256383820052d
Michael Todd <michaeltodd981@gmail.com>
2024-10-14 15:23:41 +0200

    Very Basic Tax System using Singleton and a Complaint System with different Departments

---
+++ TestingMain.cpp
@@ -0,0 +1,30 @@
+#include "TransportDepartment.h"
+#include "UtilitiesDepartment.h"
+#include "TaxDepartment.h"
+#include "Mayor.h"
+
+int main() {
+    TransportDepartment transportDept;
+    UtilitiesDepartment utilitiesDept;
+    TaxDepartment taxDept;
+    Mayor mayor;
+
+    // Set up the chain of responsibility
+    transportDept.setNextHandler(&utilitiesDept);
+    utilitiesDept.setNextHandler(&taxDept);
+    taxDept.setNextHandler(&mayor);
+
+    std::cout << "Complaint: transport\n";
+    transportDept.handleComplaint("transport");
+
+    std::cout << "\nComplaint: utilities\n";
+    transportDept.handleComplaint("utilities");
+
+    std::cout << "\nComplaint: taxes\n";
+    transportDept.handleComplaint("taxes");
+
+    std::cout << "\nComplaint: unknown issue\n";
+    transportDept.handleComplaint("unknown issue");
+
+    return 0;
+}
